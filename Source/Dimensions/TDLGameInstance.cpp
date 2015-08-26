91192689b3ce95acd4dd648c0d14d422348e9e2c
Jason Butterfield <butters_jnr@hotmail.com>
2015-08-26 10:23:56 +1000

    Actually the Data tables, Also Updated the gitignore for easier managementt

---
+++ Source/Dimensions/TDLGameInstance.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dimensions.h"
+#include "TDLGameInstance.h"
+
+TDLGameInstance::TDLGameInstance()
+{
+}
+
+TDLGameInstance::~TDLGameInstance()
+{
+}
